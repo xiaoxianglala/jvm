1761fe70512e713a4bbab97e6b6aa734fa108f35
xiaoxianglala <1143409994@qq.com>
2020-06-15 18:04:58 +0800

    java CountDownLatch

---
+++ src/main/java/com/ysy/extthread/Customer.java
@@ -0,0 +1,85 @@
+package com.ysy.extthread;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @ClassName Customer
+ * @Description TODO
+ * @Author ysy
+ * @Date 2020/6/15 17:44
+ **/
+public class Customer implements Runnable {
+    private CountDownLatch latch;
+    private String name;
+
+    public Customer(CountDownLatch latch,String name){
+        this.latch = latch;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss.SS");
+            Random random = new Random();
+
+            System.out.println(format.format(new Date()) + name + "出发去饭店");
+            Thread.sleep((long) (random.nextDouble() * 3000) + 1000);
+            System.out.println(format.format(new Date()) + name + "到达饭店");
+            latch.countDown();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class Waitress implements Runnable{
+
+    private CountDownLatch latch;
+    private String name;
+
+    public Waitress(CountDownLatch latch,String name){
+        this.latch = latch;
+        this.name = name;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss.SS");
+            System.out.println(format.format(new Date()) + name + "等待顾客");
+            latch.await();
+            System.out.println(format.format(new Date()) + name + "开始上菜");
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class T_0Run{
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(3);
+        List<Thread> threads = new ArrayList<Thread>(3);
+        threads.add(new Thread(new Customer(latch,"李四")));
+        threads.add(new Thread(new Customer(latch,"王五")));
+        threads.add(new Thread(new Customer(latch,"张三")));
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        new Thread(new Waitress(latch,"大红")).start();
+
+        //其实就是为了让主程序最后执行，join方法就是等调用其线程执行之后，再执行
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println("我是大佬");
+    }
+}
